268ae396bfa52404a9bc5842ce4e78fc60e88e54
iamsuba <subakumaran@live.com>
2021-04-16 21:08:09 -0700

    sign in and sign up page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Projects from './pages/Projects'
 import Config from './config.js'
 import ProjectDetailed from './pages/ProjectDetailed'
 import Simulator from './pages/Simulator'
+import SignIn from './pages/SignIn'
 
 function App() {
 
@@ -17,6 +18,9 @@ function App() {
         </Route>
         <Route exact path="/projects">
           <Projects data={Config.projects} />
+        </Route>
+        <Route exact path="/">
+          <SignIn />
         </Route>
         <Route path='/projectDetailed' component={ProjectDetailed} />
         <Route path='/simulator' component={Simulator} />
---
+++ src/pages/SignIn.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import styles from './SignIn.module.scss'
+import Footer from '../components/Footer'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import LogoColor from '../assets/images/logo_color.svg'
+import TextInput from '../components/Inputs/TextInput';
+import ButtonElement from '../components/ButtonElement'
+import ImageUploadInput from '../components/Inputs/ImageUploadInput'
+
+function SignIn(props) {
+
+    const [mode, setMode] = useState('signin')
+
+    const SignInContainer = 
+        <div className={styles.singIn}>
+            <Row>
+                <Col md={5} className={styles.leftContainer}>
+                    <Image src={LogoColor} height="20px" />
+                    <div className={styles.desc}>Sign or create new account to create and participate in creative projects.</div>
+                </Col>                    
+                <Col md={{ span: 6, offset: 1 }} className={styles.rightContainer}>
+                    <div className={styles.title}>Sign In</div>
+                    <div className={styles.formContainer}>
+                        <div className={styles.item}>
+                            <TextInput type="text" label="Username" placeholder="eg: johdndoe" />
+                        </div>
+                        <div className={styles.item}>
+                            <TextInput type="password" label="Password" placeholder="password" />
+                        </div>
+                        <div className={styles.item}>
+                            <ButtonElement variant="primary" block={true} label="Sign In" onClick={() => alert('button called')} />
+                            <ButtonElement variant="link" block={true} label="Create a new account" onClick={() => setMode('signup')} />
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </div>
+    
+    const SingUpContainer = 
+        <div className={styles.signUp}>
+            <Image src={LogoColor} height="25px" />
+            <div className={styles.title}>Create a new account</div>
+            <div className={styles.formContainer}>
+                <Row>
+                    <Col md={6}>
+                        <div className={styles.label}>Profile Picture</div>
+                        <ImageUploadInput />
+                        <div className={styles.item}>
+                            <TextInput type="text" label="Blockchain Address" placeholder="eg: 0xC6C48AbA7e6FF0CF04fADc1CE75Ca8FBdF8E9753" />
+                        </div>
+                    </Col>
+                    <Col md={6}>
+                        <div className={styles.item}>
+                            <TextInput type="text" label="Username" placeholder="eg: jodhndoe" />
+                        </div>
+                        <div className={styles.item}>
+                            <TextInput type="email" label="Email Address" placeholder="eg: johndoe@mail.com" />
+                        </div>
+                        <div className={styles.item}>
+                            <TextInput type="password" label="Password" placeholder="password" />
+                        </div>
+                        <div className={styles.item}>
+                            <TextInput type="password" label="Confirm Password" placeholder="password" />
+                        </div>
+                    </Col>
+                    <div className={styles.buttonsContainer}>
+                        <div className={styles.item}>
+                            <ButtonElement variant="primary" block={true} label="Create" onClick={() => alert('button called')} />
+                        </div>
+                        <div className={styles.item}>
+                            <ButtonElement variant="secondary" block={true} label="Cancel" onClick={() => setMode('signin')} />
+                        </div>
+                    </div>
+                </Row>
+            </div>
+        </div>
+
+    return(
+        <div className={styles.signInContainer}>
+            <Container>
+                { mode == "signin" ? SignInContainer : SingUpContainer }
+            </Container>
+        </div>    
+    )
+}
+
+export default SignIn
